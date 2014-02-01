27139543e0e37d26b1685d0d0337bc43dc1ad7a6
Sven Jacobs <mail@svenjacobs.com>
2014-02-01 17:41:51 +0100

    Use ScriptInjector for JS resources (required for SuperDevMode)

--- src/main/java/org/gwtbootstrap3/extras/slider/client/ui/base/SliderBase.java
+++ src/main/java/org/gwtbootstrap3/extras/slider/client/ui/base/SliderBase.java
@@ -9,9 +9,9 @@ package org.gwtbootstrap3.extras.slider.client.ui.base;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,14 +20,6 @@ package org.gwtbootstrap3.extras.slider.client.ui.base;
  * #L%
  */
 
-import org.gwtbootstrap3.client.ui.HasId;
-import org.gwtbootstrap3.client.ui.HasResponsiveness;
-import org.gwtbootstrap3.client.ui.TextBox;
-import org.gwtbootstrap3.extras.slider.client.ui.base.constants.HandleType;
-import org.gwtbootstrap3.extras.slider.client.ui.base.constants.OrientationType;
-import org.gwtbootstrap3.extras.slider.client.ui.base.constants.SelectionType;
-import org.gwtbootstrap3.extras.slider.client.ui.base.constants.TooltipType;
-
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.dom.client.Element;
@@ -35,11 +27,14 @@ import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.ui.HasEnabled;
-import com.google.gwt.user.client.ui.HasValue;
-import com.google.gwt.user.client.ui.HasVisibility;
-import com.google.gwt.user.client.ui.UIObject;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.*;
+import org.gwtbootstrap3.client.ui.HasId;
+import org.gwtbootstrap3.client.ui.HasResponsiveness;
+import org.gwtbootstrap3.client.ui.TextBox;
+import org.gwtbootstrap3.extras.slider.client.ui.base.constants.HandleType;
+import org.gwtbootstrap3.extras.slider.client.ui.base.constants.OrientationType;
+import org.gwtbootstrap3.extras.slider.client.ui.base.constants.SelectionType;
+import org.gwtbootstrap3.extras.slider.client.ui.base.constants.TooltipType;
 
 /**
  * @author Grant Slender
