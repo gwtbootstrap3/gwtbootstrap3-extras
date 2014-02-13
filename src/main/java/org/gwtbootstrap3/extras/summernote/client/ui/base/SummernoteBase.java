ed1f7eaaea1cde3f19714b92e8d0a160ea7d2d1e
jgodi <joshuagodi@gmail.com>
2014-02-13 10:29:30 -0600

    Adding in the missing events for Summernote.

--- src/main/java/org/gwtbootstrap3/extras/summernote/client/ui/base/SummernoteBase.java
+++ src/main/java/org/gwtbootstrap3/extras/summernote/client/ui/base/SummernoteBase.java
@@ -22,7 +22,10 @@ package org.gwtbootstrap3.extras.summernote.client.ui.base;
 
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.Event;
+import com.google.web.bindery.event.shared.HandlerRegistration;
 import org.gwtbootstrap3.client.ui.Div;
+import org.gwtbootstrap3.extras.summernote.client.event.*;
 
 /**
  * Wrapper for the Summernote WYSIWYG Editor
@@ -59,6 +62,34 @@ public class SummernoteBase extends Div {
         this.code = code;
     }
 
+    public HandlerRegistration addInitializedHandler(final SummernoteInitializedHandler handler) {
+        return addHandler(handler, SummernoteInitializedEvent.getType());
+    }
+
+    public HandlerRegistration addBlurHandler(final SummernoteOnBlurHandler handler) {
+        return addHandler(handler, SummernoteOnBlurEvent.getType());
+    }
+
+    public HandlerRegistration addEnterHandler(final SummernoteOnEnterHandler handler) {
+        return addHandler(handler, SummernoteOnEnterEvent.getType());
+    }
+
+    public HandlerRegistration addFocusHandler(final SummernoteOnFocusHandler handler) {
+        return addHandler(handler, SummernoteOnFocusEvent.getType());
+    }
+
+    public HandlerRegistration addImageUploadHandler(final SummernoteOnImageUploadHandler handler) {
+        return addHandler(handler, SummernoteOnImageUploadEvent.getType());
+    }
+
+    public HandlerRegistration addKeyDownHandler(final SummernoteOnKeyDownHandler handler) {
+        return addHandler(handler, SummernoteOnKeyDownEvent.getType());
+    }
+
+    public HandlerRegistration addKeyUpHandler(final SummernoteOnKeyUpHandler handler) {
+        return addHandler(handler, SummernoteOnKeyUpEvent.getType());
+    }
+
     /**
      * Gets the HTML code generated from the editor
      *
@@ -91,8 +122,6 @@ public class SummernoteBase extends Div {
     protected void onLoad() {
         super.onLoad();
 
-        // TODO - still need to sink events - jgodi
-
         // Initialize
         initialize();
     }
@@ -105,11 +134,62 @@ public class SummernoteBase extends Div {
         destroy(getElement());
     }
 
+    protected void onInitialize(final Event evt) {
+        fireEvent(new SummernoteInitializedEvent(this, evt));
+    }
+
+    protected void onBlue(final Event evt) {
+        fireEvent(new SummernoteOnBlurEvent(this, evt));
+    }
+
+    protected void onEnter(final Event evt) {
+        fireEvent(new SummernoteOnEnterEvent(this, evt));
+    }
+
+    protected void onFocus(final Event evt) {
+        fireEvent(new SummernoteOnFocusEvent(this, evt));
+    }
+
+    protected void onImageUpload(final Event evt) {
+        fireEvent(new SummernoteOnImageUploadEvent(this, evt));
+    }
+
+    protected void onKeyUp(final Event evt) {
+        fireEvent(new SummernoteOnKeyUpEvent(this, evt));
+    }
+
+    protected void onKeyDown(final Event evt) {
+        fireEvent(new SummernoteOnKeyDownEvent(this, evt));
+    }
+
     private native void initialize(Element e, int height, boolean hasFocus, JavaScriptObject toolbar) /*-{
+        var target = this;
+
         $wnd.jQuery(e).summernote({
             height: height,
             focus: hasFocus,
-            toolbar: toolbar
+            toolbar: toolbar,
+            oninit: function(evt) {
+                target.@org.gwtbootstrap3.extras.summernote.client.ui.base.SummernoteBase::onInitialize(Lcom/google/gwt/user/client/Event;)(evt);
+            },
+            onenter: function(evt) {
+                target.@org.gwtbootstrap3.extras.summernote.client.ui.base.SummernoteBase::onEnter(Lcom/google/gwt/user/client/Event;)(evt);
+            },
+            onfocus: function(evt) {
+                target.@org.gwtbootstrap3.extras.summernote.client.ui.base.SummernoteBase::onFocus(Lcom/google/gwt/user/client/Event;)(evt);
+            },
+            onblur: function(evt) {
+                target.@org.gwtbootstrap3.extras.summernote.client.ui.base.SummernoteBase::onBlue(Lcom/google/gwt/user/client/Event;)(evt);
+            },
+            onkeyup: function(evt) {
+                target.@org.gwtbootstrap3.extras.summernote.client.ui.base.SummernoteBase::onKeyUp(Lcom/google/gwt/user/client/Event;)(evt);
+            },
+            onkeydown: function(evt) {
+                target.@org.gwtbootstrap3.extras.summernote.client.ui.base.SummernoteBase::onKeyDown(Lcom/google/gwt/user/client/Event;)(evt);
+            },
+            onImageUpload: function(evt) {
+                target.@org.gwtbootstrap3.extras.summernote.client.ui.base.SummernoteBase::onImageUpload(Lcom/google/gwt/user/client/Event;)(evt);
+            }
         });
     }-*/;
 
