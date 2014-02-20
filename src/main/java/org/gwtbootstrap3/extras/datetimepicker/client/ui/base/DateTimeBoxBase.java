1886b674aef5874de4b1af063c4a2c8b570bca42
jgodi <joshuagodi@gmail.com>
2014-02-19 21:09:08 -0600

    Removing focus from the textbox after onHide is fired and giving focus when onShow is fired. (Issue #13)

--- src/main/java/org/gwtbootstrap3/extras/datetimepicker/client/ui/base/DateTimeBoxBase.java
+++ src/main/java/org/gwtbootstrap3/extras/datetimepicker/client/ui/base/DateTimeBoxBase.java
@@ -108,12 +108,10 @@ public class DateTimeBoxBase extends Widget implements HasValue<Date>, HasEnable
     }
 
     public void show() {
-        textBox.setFocus(true);
         show(textBox.getElement());
     }
 
     public void hide() {
-        textBox.setFocus(false);
         hide(textBox.getElement());
     }
 
@@ -294,10 +292,14 @@ public class DateTimeBoxBase extends Widget implements HasValue<Date>, HasEnable
     }
 
     public void onShow(final Event e) {
+        // On show we put focus on the textbox
+        textBox.setFocus(true);
         fireEvent(new ShowEvent(e));
     }
 
     public void onHide(final Event e) {
+        // On hide we remove focus from the textbox
+        textBox.setFocus(false);
         fireEvent(new HideEvent(e));
     }
 
