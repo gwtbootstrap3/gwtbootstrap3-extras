0c8818fe88cadbed1b20f0b9079645f0bc01a796
BenDol <dolb90@gmail.com>
2015-10-09 19:52:01 +1300

    Added Bootstrap Image Gallery wrapper.
    https://blueimp.github.io/Bootstrap-Image-Gallery/

---
+++ src/main/java/org/gwtbootstrap3/extras/gallery/client/GalleryEntryPoint.java
@@ -0,0 +1,46 @@
+package org.gwtbootstrap3.extras.gallery.client;
+
+/*
+ * #%L
+ * GwtBootstrap3
+ * %%
+ * Copyright (C) 2013 - 2014 GwtBootstrap3
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.ScriptInjector;
+import com.google.gwt.resources.client.TextResource;
+
+/**
+ * @author Ben Dol
+    */
+public class GalleryEntryPoint implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        inject(GalleryClientBundle.INSTANCE.blueimpGallery());
+        inject(GalleryClientBundle.INSTANCE.blueimpJQuery());
+        inject(GalleryClientBundle.INSTANCE.fullscreen());
+        inject(GalleryClientBundle.INSTANCE.indicator());
+        inject(GalleryClientBundle.INSTANCE.bootstrapGallery());
+    }
+
+    private void inject(TextResource resource) {
+        ScriptInjector.fromString(resource.getText())
+            .setWindow(ScriptInjector.TOP_WINDOW)
+            .inject();
+    }
+}
