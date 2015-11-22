ea2fdcebe2c535d084c0129313b45042a1dcb391
Xiaodong Sun <xiaodong.sun@wallix.com>
2015-11-22 10:51:34 +0100

    Update bootstrap slider to 5.2.6

--- src/main/java/org/gwtbootstrap3/extras/slider/client/SliderClientBundle.java
+++ src/main/java/org/gwtbootstrap3/extras/slider/client/SliderClientBundle.java
@@ -9,9 +9,9 @@ package org.gwtbootstrap3.extras.slider.client;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,6 +31,6 @@ interface SliderClientBundle extends ClientBundle {
 
     static final SliderClientBundle INSTANCE = GWT.create(SliderClientBundle.class);
 
-    @Source("resource/js/bootstrap-slider-5.1.1.min.cache.js")
+    @Source("resource/js/bootstrap-slider-5.2.6.min.cache.js")
     TextResource slider();
 }
