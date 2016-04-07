6b9c06ecb977b48649e1657379b280f8358e1f9d
Xiaodong Sun <xiaodong.sun@wallix.com>
2016-04-07 23:39:04 +0200

    Updated slider to 7.0.1

--- src/main/java/org/gwtbootstrap3/extras/slider/client/ui/base/SliderBase.java
+++ src/main/java/org/gwtbootstrap3/extras/slider/client/ui/base/SliderBase.java
@@ -881,7 +881,7 @@ public abstract class SliderBase<T> extends Widget implements
 
     /**
      * FIXME: This is a workaround for the refresh command, since it is buggy in
-     * the current version (6.1.8). After executing this command, the slider
+     * the current version (7.0.1). After executing this command, the slider
      * becomes consistently a range slider with 2 handles. This should be
      * removed once the bug is fixed in a future version.
      *
